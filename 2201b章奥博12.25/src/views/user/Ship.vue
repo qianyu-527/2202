ebd014a648986c1aaa3a1a17f4c1d0429021cfd2
qianyu <3106277748@qq.com>
2022-12-25 23:52:23 +0800

    修改

---
+++ 2201b章奥博12.25/src/views/user/Ship.vue
@@ -0,0 +1,65 @@
+<template>
+<div>
+    <div id="main" style="width: 300px;height:150px;"></div>
+</div>
+</template>
+
+<script>
+import * as echarts from 'echarts';
+
+export default {
+  name: '',
+  components: {},
+  data () {
+    return {
+    }
+  },
+  created () { },
+  mounted () { 
+    var myChart = echarts.init(document.getElementById('main'));
+    var option;
+
+option = {
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'shadow'
+    }
+  },
+  grid: {
+    left: '3%',
+    right: '4%',
+    bottom: '3%',
+    containLabel: true
+  },
+  xAxis: [
+    {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      axisTick: {
+        alignWithLabel: true
+      }
+    }
+  ],
+  yAxis: [
+    {
+      type: 'value'
+    }
+  ],
+  series: [
+    {
+      name: 'Direct',
+      type: 'bar',
+      barWidth: '60%',
+      data: [10, 52, 200, 334, 390, 330, 220]
+    }
+  ]
+};
+option && myChart.setOption(option);
+  },
+  methods: {}
+}
+</script>
+<style scoped lang='scss'>
+
+</style>
