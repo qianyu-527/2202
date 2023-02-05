60e7e4b12bbec2b25d86773c1f9263cd37b01d53
qianyu <3106277748@qq.com>
2023-02-05 15:04:11 +0800

    修改

--- 2.1作业/2.1后台作业/src/api/login.js
+++
@@ -1,9 +0,0 @@
-import http from '../untils/http'
-// 请求登录
-export function Login(params){
-	return http({
-		url:http.apis("/sys/login"),
-		method:"POST",
-		params
-	})
-}
--- 2.2作业/demo/src/api/login.js
+++
@@ -1,9 +0,0 @@
-import http from '../untils/http'
-// 请求登录
-export function Login(data){
-	return http({
-		url:http.apis("/sys/login"),
-		method:"POST",
-		data
-	})
-}
